04be89e70561d1d8b1c616153fa3ffe7ff2355cb
Aakash Sharma <akash.sharma@iesoftek.com>
2024-02-27 12:27:45 +0530

    Weather App deployed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FetchApiData from './FetchApiData';
+
+function App() {
+  return (
+    <>
+    <FetchApiData />
+    </>
+  );
+}
+
+export default App;
---
+++ src/FetchApiData.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import Weathercard from "./Weathercard";
+import "./Style.css";
+
+const FetchApiData = () => {
+  const [searchValue, setSearchValue] = useState("pune");
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=7486fc54bcb0d2fc2f13748fa2791050&units=metric`;
+
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { temp, humidity, pressure } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
+
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* our card  */}
+      <Weathercard {...tempInfo} />
+    </>
+  );
+};
+
+export default FetchApiData;
